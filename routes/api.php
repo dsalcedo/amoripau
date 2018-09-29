b6a8c472ae49af54243d426d86161816d45c297e
Daniel Salcedo <daniel.salcedo@konfio.mx>
2018-09-29 13:39:02 -0500

    Fix session refresh

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::group(['namespace'=>'api', 'middleware' => ['sessions']], function($r) {
-
-    $r->post('/add/shopping/cart','ShoppingCartController@add')->name('shoping.cart.add');
-    $r->get('/show/shopping/cart','ShoppingCartController@show')->name('shoping.cart.show');
-
-});
-
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/detalle/{producto_id}','TiendaController@view')->name('detalle');
 Auth::routes();
 
+Route::group(['namespace'=>'api', 'prefix' => 'internal'], function($r) {
+
+    $r->post('/add/shopping/cart','ShoppingCartController@add')->name('shoping.cart.add');
+    $r->get('/show/shopping/cart','ShoppingCartController@show')->name('shoping.cart.show');
+
+});
